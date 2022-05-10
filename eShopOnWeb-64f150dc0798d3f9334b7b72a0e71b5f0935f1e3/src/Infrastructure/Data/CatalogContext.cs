7913c5bfea79afea0f150bc72b54c060cac75c01
Ghiles K <1584887@cegepmontpetit.ca>
2022-05-10 02:29:29 -0400

    eshop

---
+++ eShopOnWeb-64f150dc0798d3f9334b7b72a0e71b5f0935f1e3/src/Infrastructure/Data/CatalogContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Reflection;
+
+
+
+namespace Microsoft.eShopWeb.Infrastructure.Data
+{
+
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "server=localhost;port=3306;user=root;password=allo123;database=ef";
+            var serverVersion = new MariaDbServerVersion(new Version(8, 0, 27));
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql(connectionString, serverVersion);
+            }  
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
